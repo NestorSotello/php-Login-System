aa940a865877e8eca9ee0143daf80a3649f79130
Nestor Sotelo <nestorfabiansotello@gmail.com>
2020-02-26 15:18:20 -0300

    solucionado la conexion a la bd

--- database/01-setup.sql
+++ database/01-setup.sql
@@ -7,8 +7,10 @@
 -- Server version: 5.7.19-0ubuntu0.16.04.1
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+03:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,8 +21,11 @@ SET time_zone = "+00:00";
 --
 -- Database: `login_course`
 --
-CREATE DATABASE IF NOT EXISTS `login_course` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `login_course`;
+CREATE DATABASE
+IF NOT EXISTS 'login_system' DEFAULT CHARACTER
+SET utf8mb4
+COLLATE utf8mb4_general_ci;
+USE 'login_system';
 
 -- --------------------------------------------------------
 
@@ -29,10 +34,14 @@ USE `login_course`;
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `user_id` int(5) UNSIGNED NOT NULL COMMENT 'User Id',
-  `email` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Users email address',
-  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User password',
+CREATE TABLE `users`
+(
+  `user_id` int
+(5) UNSIGNED NOT NULL COMMENT 'User Id',
+  `email` varchar
+(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Users email address',
+  `password` varchar
+(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User password',
   `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The time and date the user registered'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Users table';
 
@@ -49,8 +58,10 @@ TRUNCATE TABLE `users`;
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
+ADD PRIMARY KEY
+(`user_id`),
+ADD UNIQUE KEY `email`
+(`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -60,7 +71,8 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id';
+  MODIFY `user_id` int
+(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id';
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
